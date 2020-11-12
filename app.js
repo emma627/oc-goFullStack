3988a6ab1cf409977075db0235556d2f697c1a88
jinyan yang <jinyan.yang627@gmail.com>
2020-11-12 11:13:14 +0100

    Développez la fonction delete du back-end

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://user123:utilisateur123@cluster0.h4n1u.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,8 +19,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
 app.use(bodyParser.json());
+
 app.use('/api/stuff', stuffRoutes);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
